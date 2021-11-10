dd494e965ae2822d35481f3fa03045991e43a8c5
sdkskdks <84399320+sdkskdks@users.noreply.github.com>
2021-11-10 22:52:19 +0600

    Update database.py

--- database.py
+++ database.py
@@ -3,7 +3,6 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:<password>@localhost/<database_name>'
-# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 class Tableone(db.Model):
